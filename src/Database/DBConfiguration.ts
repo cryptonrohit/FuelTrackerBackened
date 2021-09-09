88334aea6eb7478d847e83cceecf5375def8d2e1
z003w4hv <rohit.pal@siemens.com>
2021-09-09 10:12:05 +0530

    feat: db singleton instance creation

--- src/Database/DBConfiguration.ts
+++ src/Database/DBConfiguration.ts
@@ -1,4 +1,18 @@
+import { Knex } from "knex";
+import dbinitConnection from "./dbInitConnection";
+
 class DBConfiguration {
+    public dbConnector: Knex;
+    private static dbInstance: DBConfiguration;
+    constructor() {
+        this.dbConnector = dbinitConnection.connectionToDB();
+    }
+    public static DBInstance(): DBConfiguration {
+        if (!DBConfiguration.dbInstance) {
+            return new DBConfiguration;
+        }
+        return DBConfiguration.dbInstance;
+    }
 
     async init(){
         // call to create tables
